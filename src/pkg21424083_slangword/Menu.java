7200624125e8993cf423fb5ef4d3e125ec0840a2
unknown <lehquy99@gmail.com>
2022-07-05 18:21:35 +0700

    Add menu

---
+++ src/pkg21424083_slangword/Menu.java
@@ -0,0 +1,134 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg21424083_slangword;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
+
+/**
+ *
+ * @author LeQuy
+ */
+public class Menu extends JFrame implements ActionListener {
+    JButton btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8;
+	SlangWord slangWord;
+        UIManager manager=new UIManager();
+        LinkedList<Object> a =new LinkedList<Object>();
+	Menu() {
+		slangWord = SlangWord.getInstance();
+                  
+                    a.add(0.3);
+                    a.add(0.3);
+
+                    //First colour : R=2,G=208,B=206
+                    a.add(new ColorUIResource(2,208,206));
+
+                    //Second colour : R=136,G=255,B=254
+                    a.add(new ColorUIResource(136,255,254));
+
+                    //Third colour : R=0,G=142,B=140
+                    a.add(new ColorUIResource(0,142,140));
+		JLabel label = new JLabel("Slang Words");
+		label.setForeground(Color.green);
+		label.setFont(new Font("NewellsHand", Font.PLAIN, 35));
+		label.setAlignmentX(CENTER_ALIGNMENT);
+                manager.put("Button.gradient",a);
+		// label.addMouseListener(this);
+		// A Grid
+		btn1 = new JButton("1. Danh sach Slang word");
+		btn1.addActionListener(this);
+		btn1.setFont(new Font("NewellsHand", Font.PLAIN, 14));
+		btn1.setFocusable(false);
+
+		btn2 = new JButton("2. Tim Slang word");
+		btn2.addActionListener(this);
+		btn2.setFont(new Font("NewellsHand", Font.PLAIN, 14));
+		btn2.setFocusable(false);
+
+		btn3 = new JButton("3. Them slang word");
+		btn3.addActionListener(this);
+		btn3.setFont(new Font("Gill Sans MT", Font.PLAIN, 14));
+		btn3.setFocusable(false);
+
+		btn4 = new JButton("4. Random Slang Words");
+		btn4.addActionListener(this);
+		btn4.setFont(new Font("Gill Sans MT", Font.PLAIN, 14));
+		btn4.setFocusable(false);
+
+		btn5 = new JButton("5. Lich su");
+		btn5.addActionListener(this);
+		btn5.setFont(new Font("Gill Sans MT", Font.PLAIN, 14));
+		btn5.setFocusable(false);
+
+		btn6 = new JButton("6. Xoa Slang Word");
+		btn6.addActionListener(this);
+		btn6.setFont(new Font("Gill Sans MT", Font.PLAIN, 14));
+		btn6.setFocusable(false);
+
+		btn7 = new JButton("7. Reset Slang Word");
+		btn7.addActionListener(this);
+		btn7.setFont(new Font("Gill Sans MT", Font.PLAIN, 14));
+		btn7.setFocusable(false);
+
+		btn8 = new JButton("8. Quiz");
+		btn8.addActionListener(this);
+		btn8.setFont(new Font("Gill Sans MT", Font.PLAIN, 14));
+		btn8.setFocusable(false);
+
+		JPanel panelCenter = new JPanel();
+		// panelCenter.setBackground(Color.gray);
+		panelCenter.setLayout(new GridLayout(3, 3, 10, 10));
+		panelCenter.add(btn1);
+		panelCenter.add(btn2);
+		panelCenter.add(btn3);
+		panelCenter.add(btn4);
+		panelCenter.add(btn5);
+		panelCenter.add(btn6);
+		panelCenter.add(btn7);
+		panelCenter.add(btn8);
+
+		Dimension size2 = new Dimension(800, 600);
+		panelCenter.setMaximumSize(size2);
+		panelCenter.setPreferredSize(size2);
+		panelCenter.setMinimumSize(size2);
+		Container con = this.getContentPane();
+		con.setLayout(new BoxLayout(con, BoxLayout.Y_AXIS));
+		con.add(Box.createRigidArea(new Dimension(0, 10)));
+		con.add(label);
+		con.add(Box.createRigidArea(new Dimension(0, 30)));
+		con.add(panelCenter);
+
+		// Setting Frame
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Menu Window");
+		this.setVisible(true);
+		this.setSize(700, 700);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+//		
+		}
+
+}
